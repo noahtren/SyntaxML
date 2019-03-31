4e4c576e09ba754b7747f5e26025f025bd880c99
noahtren <noahtren@gmail.com>
2019-03-31 00:28:07 -0400

    preparing to populate common queries with stackoverflow data

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ common_queries = [
     "infinite loop",
 
     # file handling
-    "open a file",
+    "open file",
     "read file",
     "write to file",
     "append file",
@@ -24,6 +24,7 @@ common_queries = [
     "length of string",
     "reverse a string",
     "split a string",
+    "remove item from list",
     "copy one string to another",
     "append one string to another",
     "return the length of the string",
@@ -38,16 +39,51 @@ common_queries = [
     "function that reverses a list, preferably in place",
     "function that checks whether an element occurs in a list",
     "function that returns the elements on odd positions in a list",
-    "function that computes the running total of a list"
-    
-    
-    "get substring",
-    "get index of element",
-    
+    "function that computes the running total of a list",
+    "function that tests whether a string is a palindrome",
+
+    # i/o
+    "prompt the user",
+    "print an output",   
     "write to console",
     
-    "convert integer to string",
-    "remove item from list",
+    # arrays
     "initialize array",
-    "update inner html"
+    "print shortest path",
+    "resize an array",
+
+    #JavaScript specific
+    "return the response from an asynchronous call",
+    "closure inside loops",
+    "binding on dynamically created elements",
+    "access / process (nested) objects, arrays or JSON",
+    "get query string values",
+    "this keyword",
+    "upload files asynchronously",
+    "pass variables and data from PHP to JavaScript",
+    "prop() vs attr()",
+    "redirect to another webpage",
+    "validate an email address",
+    "randomize an array",
+    "replace all occurrences of a string",
+    "copy to the clipboard",
+    "clone an object",
+    "compare two dates",
+    "detect a click outside an element",
+    "sorting an array of objects by property",
 ]
+import json
+# READING
+json_string = open("data.json").read()
+json_obj = json.loads(json_string)
+
+
+for language in json_obj["data"].keys():
+    for command in json_obj["data"][language].keys():
+        print(command)
+
+
+# WRITING
+json_obj["data"]["Python"]["for loop"]="blah blah"
+file_out = open("data.json", "w")
+file_out.write(json.dumps(json_obj))
--- request_connection.py
+++ request_connection.py
@@ -1,6 +1,5 @@
 import requests 
 from bs4 import BeautifulSoup
-#from WEB_SCRAPE_FUNCS import url_to_soup
 
 def url_to_soup(url):
     page = requests.get(url)
@@ -8,42 +7,46 @@ def url_to_soup(url):
     to_soup = BeautifulSoup(page.content, 'html.parser')
     return to_soup
 
-example_input = "Python reverse list"
-# get input from noah 
+def top_answers_stackoverflow(inp):
+    str_input = inp.split() 
+    url_query = "https://stackoverflow.com/search?q="
 
-"""content = example_input.split(" ")
-to_soup = 'https://stackoverflow.com/search?q='
+    for ii in str_input:
+        url_query += str_input[ii]
+        url_query += '+'
 
-for ii in content:
-    to_soup += content[ii]
-    to_soup += '+'
 
-to_soup = url_to_soup(to_soup)"""
+   # to_soup = url_to_soup('https://stackoverflow.com/search?q=python+reverse+list')
+    # starting to sift through the search results 
 
-to_soup = url_to_soup('https://stackoverflow.com/search?q=python+reverse+list')
-# starting to sift through the search results 
+    # finding all of the search results
+    answers = url_query.find_all("div", {"class":"question-summary search-result"})#find all 
 
-# finding all of the search results
-answers = to_soup.find_all("div", {"class":"question-summary search-result"})
+    # checking for the conditions of if answered, if answered go to url and check for green check and code 
 
-has_answers = []
-# checking for the conditions of if answered, if answered go to url and check for green check and code 
-for answer in answers:
-    has_answers.extend(answer.find_all("div", {"class":"status answered-accepted"})) 
-    
-    # if there are answers check each webpage for check mark and if there's a code block
+    a_i = 0
 
-    # opening web page to check 
-    link_find = answer.find("a", {"class":"question-hyperlink"})
-    
-    link = 'https://stackoverflow.com/' + link_find['href']
+    for answer in answers:
+        a_i += 1
+        if a_i == 4:
+            break
+        # if there are answers check each webpage for check mark and if there's a code block
+        # opening web page to check 
+        link_find = answer.find("a", {"class":"question-hyperlink"})
+        link = 'https://stackoverflow.com/' + link_find['href']
+        print("Querying link: {}".format(link))
 
-    to_check = url_to_soup(link)
+        answer_soup = url_to_soup(link)
 
-    # find check mark 
-    checked = to_check.find("div", {"class":"lang-py prettyprint prettyprinted"})
-    print(checked)
-
-    # check for all <code> tags in the html
-    code = to_check.findAll("code")
-    print(code)
+        # find check mark 
+        checked = answer_soup.find("div", {"class":"accepted-answer"})
+        #print(checked)
+        if checked != None:
+            code = checked.find("div", {"class":"post-text"})
+            code_samples = code.findAll("code")
+            for code in code_samples:
+                print(code.prettify())
+        else:
+            continue
+    return "here's the top code sample"
+        
